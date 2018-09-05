63e8741dec9c954dd9ce4ee2245fbbd6df4530b7
YellowFive5 <yellowfive555@yahoo.com>
2018-09-05 13:41:44 +0300

    [C# Essential] Namespaces,PreprocessorDerectives

---
+++ _1_Conso1e/_1_Conso1e/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Another   //  Другое пространство имен в решении
+{
+    class Class1    //  Класс в другом просранстве имен
+    {
+
+    }
+}
